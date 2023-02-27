b8662ef346ae490cdc22ae65591be6099f7524b9
MuraliD <chandusaimurali007@gmail.com>
2023-02-27 20:13:37 +0530

    Revert "Made Changes to support 2 dim arrays."
    This reverts commit 67c02ff8a4b25d9e6bc45a6b9f487f58cd779328.

--- LinearReg_Gradient_Scratch.py
+++ LinearReg_Gradient_Scratch.py
@@ -3,11 +3,11 @@ from sklearn.model_selection import train_test_split
 import warnings
 
 class SimpleLR:
-    def __init__(self, alpha=0.001, n_iter=50, random_state = None):
+    def __init__(self, alpha=0.01, n_iter=50, random_state = None):
         self.alpha = alpha
         self.n_iter = n_iter
-        self.m = None
-        self.b = None
+        self.m = np.random.ranf(1)[0]
+        self.b = np.random.ranf(1)[0]
         self.X = None
         self.y = None
         self.y_pred = None
@@ -30,48 +30,45 @@ class SimpleLR:
         
     
     def update_weights(self):
-        dl_dm = -2*(self.X.T.dot((self.y-self.m*self.X-self.b))).sum(axis=1)/self.y.shape[0]
-        dl_dm = np.where(dl_dm>50,50,dl_dm)
-        dl_dm = np.where(dl_dm<-50,-50,dl_dm)
-        dl_db = -2*((self.y-self.m*self.X-self.b)).sum(axis=0)/self.y.shape[0]
-        dl_db = np.where(dl_db>50,50,dl_db)
-        dl_db = np.where(dl_db<-50,-50,dl_db)
-        
+        dl_dm = -2*(self.X.dot((self.y-self.m*self.X-self.b))).sum()/self.y.shape[0]
+        if dl_dm > 1 : dl_dm = 1
+        if dl_dm < -1: dl_dm = -1
+        dl_db = -2*((self.y-self.m*self.X-self.b)).sum()/self.y.shape[0]
+        if dl_db > 1 : dl_db = 1
+        if dl_db < -1: dl_db = -1
         self.m = self.m - (dl_dm*self.alpha)
         self.b = self.b - (dl_db*self.alpha)
     
     def fit(self,X,y):
         self.X = X
         self.y = y
-        self.m = np.random.randn(X.shape[1]).reshape(1,-1)
-        self.b = np.random.randn(X.shape[1]).reshape(1,-1)
-        self.y_pred = (self.m*self.X+self.b).sum(axis=1).reshape(-1,1)
+        self.y_pred = self.m*self.X+self.b
+        print(self.loss())
         for i in range(self.n_iter):
             self.update_weights()
-            self.y_pred = (self.m*self.X+self.b).sum(axis=1).reshape(-1,1)
-            # print("Cycle {0}: m:".format({i+1}),self.m,"b:",self.b,"loss:",self.loss())
+            self.y_pred = self.m*self.X+self.b
+            print("Cycle {0}: m:".format({i+1}),self.m,"b:",self.b,"loss:",self.loss())
         print(self.y)
         print(self.y_pred)
-        print(self.loss())
         # print(y_pred)
         # print(y)
         
     def predict(self, X):
         return self.m*X+self.b
     
-# warnings.filterwarnings('ignore')    
+warnings.filterwarnings('ignore')    
 
-# lr = SimpleLR(random_state=None, n_iter=1000)
-# X = np.arange(1,1000)
-# y = X*5+5
-# print(y.shape[0])
-# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=1)   
+lr = SimpleLR(random_state=None, n_iter=1000)
+X = np.arange(1,1000)
+y = X*5+5
+print(y.shape[0])
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=1)   
 
-# lr.fit(X_train, y_train)
+lr.fit(X_train, y_train)
 
-# print(lr.score(X_train, y_train))
-# print(lr.score(X_test, y_test))
+print(lr.score(X_train, y_train))
+print(lr.score(X_test, y_test))
 
-# print(lr.predict(X_test))
-# print(y_test)    
+print(lr.predict(X_test))
+print(y_test)    
     
