c1e19cf58fe8a57e3ff82c0f4c45ee35e84e01dd
MuraliD <chandusaimurali007@gmail.com>
2023-02-28 11:26:23 +0530

    updated n-dim code

--- LinearReg_Gradient_Scratch.py
+++ LinearReg_Gradient_Scratch.py
@@ -1,9 +1,8 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
 import warnings
 
 class SimpleLR:
-    def __init__(self, alpha=0.001, n_iter=50, random_state = None):
+    def __init__(self, alpha=0.01, n_iter=50, random_state = None):
         self.alpha = alpha
         self.n_iter = n_iter
         self.m = None
@@ -22,42 +21,43 @@ class SimpleLR:
         return self.m, self.b 
     
     def score(self,X,y):
-        self.y_pred = self.m*X+self.b
-        ssr = np.square(y-self.y_pred).sum()
-        self.y_pred = self.y.mean()
-        sst =  np.square(y-self.y_pred).sum()
+        y_pred = (self.m*X+self.b).sum(axis=1).reshape(-1,1)
+        ssr = np.square(y-y_pred).sum()
+        y_pred = y.mean()
+        sst =  np.square(y-y_pred).sum()
         return 1 - (ssr/sst)
         
     
     def update_weights(self):
-        dl_dm = -2*(self.X.T.dot((self.y-self.m*self.X-self.b))).sum(axis=1)/self.y.shape[0]
+        dl_dm = -((self.X*(self.y-self.m*self.X-self.b)).sum(axis=0))/self.y.shape[0]
         dl_dm = np.where(dl_dm>50,50,dl_dm)
         dl_dm = np.where(dl_dm<-50,-50,dl_dm)
-        dl_db = -2*((self.y-self.m*self.X-self.b)).sum(axis=0)/self.y.shape[0]
+        dl_db = -((self.y-self.m*self.X-self.b).sum())/self.y.shape[0]
         dl_db = np.where(dl_db>50,50,dl_db)
         dl_db = np.where(dl_db<-50,-50,dl_db)
         
         self.m = self.m - (dl_dm*self.alpha)
         self.b = self.b - (dl_db*self.alpha)
     
-    def fit(self,X,y):
+    def fit(self,X,y,verbose = True):
         self.X = X
         self.y = y
-        self.m = np.random.randn(X.shape[1]).reshape(1,-1)
-        self.b = np.random.randn(X.shape[1]).reshape(1,-1)
+        self.m = np.random.randn(X.shape[1]).reshape(1,-1)*np.sqrt(2/self.y.shape[0])
+        self.b = np.random.randn(1)*np.sqrt(2/self.y.shape[0])
         self.y_pred = (self.m*self.X+self.b).sum(axis=1).reshape(-1,1)
         for i in range(self.n_iter):
             self.update_weights()
             self.y_pred = (self.m*self.X+self.b).sum(axis=1).reshape(-1,1)
-            # print("Cycle {0}: m:".format({i+1}),self.m,"b:",self.b,"loss:",self.loss())
-        print(self.y)
-        print(self.y_pred)
-        print(self.loss())
+            if verbose:
+                print("Cycle {0}: m:".format({i+1}),self.m,"b:",self.b,"loss:",self.loss())
+        # print(self.y)
+        # print(self.y_pred)
+        # print(self.loss())
         # print(y_pred)
         # print(y)
         
     def predict(self, X):
-        return self.m*X+self.b
+        return (self.m*X+self.b).sum(axis=1).reshape(-1,1)
     
 # warnings.filterwarnings('ignore')    
 
